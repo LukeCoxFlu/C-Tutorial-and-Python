16d28d2b6b0df55d031052a4def654ad563c82f9
Luke Cox <w9074029@live.tees.ac.uk>
2021-04-13 19:08:01 +0100

    Session1

--- TutOneNotTheDemo/Project1/main.cpp
+++ TutOneNotTheDemo/Project1/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <string>
+int s = 6;
+int howManySocksDoIHave()
+{
+	return 2;
+}
+
+int timestwovalues(int x, int y)
+{
+	return x * y;
+}
 
 int main()
 {
-	std::cout << "Yeet";
+//  ===========================================================================
+	const int a = 6;
+	char b = 'K';
+	std::string B = "yEET aksduhiauh askjdhialsuda aosidjasidjoijia dasdhasd";
+	float i = 9.89;
+	bool o = true;
+//  ===========================================================================
+	int K = s;
+//  ===========================================================================
+
+//  ========    0 1 2 3  4
+	int k[5] = {8,9,4,12,3};
+	s = k[2];
+
+//  ===========================================================================
+	int sock = howManySocksDoIHave();
+	int times = timestwovalues(a, s);
+
+	std::cout << a << " X " << s << " = " << times;
 	return 0;
 }
+
